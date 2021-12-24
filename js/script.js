bd35612a3609b5f13e9c76cde1b3a7b2621e74f2
furvietto <fulvio-94@hotmail.it>
2021-12-24 14:58:06 +0100

    splice almost done

--- js/script.js
+++ js/script.js
@@ -16,16 +16,19 @@ const project = new Vue({
                   date: "10/01/2020 15:30:55",
                   text: "Hai portato a spasso il cane?",
                   status: "sent",
+                  condition: false,
                 },
                 {
                   date: "10/01/2020 15:50:00",
                   text: "Ricordati di dargli da mangiare",
                   status: "sent",
+                  condition: false,
                 },
                 {
                   date: "10/01/2020 16:15:22",
                   text: "Tutto fatto!",
                   status: "received",
+                  condition: false,
                 },
               ],
             },
@@ -38,16 +41,19 @@ const project = new Vue({
                   date: "20/03/2020 16:30:00",
                   text: "Ciao come stai?",
                   status: "sent",
+                  condition: false,
                 },
                 {
                   date: "20/03/2020 16:30:55",
                   text: "Bene grazie! Stasera ci vediamo?",
                   status: "received",
+                  condition: false,
                 },
                 {
                   date: "20/03/2020 16:35:00",
                   text: "Mi piacerebbe ma devo andare a fare la spesa.",
                   status: "sent",
+                  condition: false,
                 },
               ],
             },
@@ -61,16 +67,19 @@ const project = new Vue({
                   date: "28/03/2020 10:10:40",
                   text: "La Marianna va in campagna",
                   status: "received",
+                  condition: false,
                 },
                 {
                   date: "28/03/2020 10:20:10",
                   text: "Sicuro di non aver sbagliato chat?",
                   status: "sent",
+                  condition: false,
                 },
                 {
                   date: "28/03/2020 16:15:22",
                   text: "Ah scusa!",
                   status: "received",
+                  condition: false,
                 },
               ],
             },
@@ -83,11 +92,13 @@ const project = new Vue({
                   date: "10/01/2020 15:30:55",
                   text: "Lo sai che ha aperto una nuova pizzeria?",
                   status: "sent",
+                  condition: false,
                 },
                 {
                   date: "10/01/2020 15:50:00",
                   text: "Si, ma preferirei andare al cinema",
                   status: "received",
+                  condition: false,
                 },
                 
               ],
@@ -116,6 +127,7 @@ const project = new Vue({
             date: data,
             text: this.addSend,
             status: "sent",
+            condition: false,
           })
           this.addSend = "";
 
@@ -124,6 +136,7 @@ const project = new Vue({
               date: data,
               text: "ok",
               status: "received",
+              condition: false,
             })
           }, 2000);
         }
@@ -137,7 +150,11 @@ const project = new Vue({
             element.visible = false
           }
         })
-      }
+      },
+
+      deleteMessages: function (index) {
+        this.contacts[this.counter].messages.splice(index , 1)
+      },
 
     },
     
